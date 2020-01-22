3f035706133ba99b78b72e9d98439cde752a7267
Matthew Tice <mwt160030@utdallas.edu>
2020-01-22 16:47:53 -0600

    Background update

--- js/background.js
+++ js/background.js
@@ -1,61 +1,107 @@
-var canvas = $("#bg")[0];
-var ctx = canvas.getContext("2d");
-var width = 20, left_drops, right_drops;
+var canvas_left = $("#bg_left")[0];
+var canvas_right = $("#bg_right")[0];
+var ctx_left = canvas_left.getContext("2d");
+var ctx_right = canvas_right.getContext("2d");
+var frame = 0, do_animation = true, columns, width, left_drops, right_drops, animation_id;
+const drop_spawn_rate = 0.95;
 
 function resizeCanvas() {
+    cancelAnimationFrame(animation_id);
+
+    if(/Mobi|Android/i.test(navigator.userAgent)) { 
+        do_animation = false;
+    }
+
     // Initialize drops
     let main = $("#main")[0];
-    columns = Math.floor((getComputedStyle(main).getPropertyValue("margin-left").slice(0, -2) + getComputedStyle(main).getPropertyValue("padding-left").slice(0, -2)) / width);
-    left_drops = [], right_drops = [];
+    let dpi = window.devicePixelRatio;
+    let style_height = parseInt(getComputedStyle(main).getPropertyValue("height").slice(0, -2));
+    let style_width = parseInt(getComputedStyle(main).getPropertyValue("margin-left").slice(0, -2)) + (parseInt(getComputedStyle(main).getPropertyValue("padding-left").slice(0, -2)));
+
+    var temp_width = 15, diff = 1;
+    var curr_columns = style_width * dpi / temp_width;
+    while(columns % 1 !== 0 && temp_width < 25) {
+        temp_width++;
+        curr_columns = style_width * dpi / temp_width;
+        console.log(curr_columns, Math.round(curr_columns), Math.abs(curr_columns - Math.round(curr_columns)))
+        if (Math.abs(curr_columns - Math.round(curr_columns)) <= diff) {
+            columns = curr_columns;
+            width = temp_width;
+            diff = Math.abs(curr_columns - Math.round(curr_columns));
+        }
+    }
+
+    console.log(columns, width, diff);
+
+    frame = 0, left_drops = [], right_drops = [];
 
     for (var i = 0; i < columns; i++) {
         left_drops[i] = 0;
         right_drops[i] = 0;
     }
 
-    // Adjust canvas for device 
-    let dpi = window.devicePixelRatio;
-    let style_height = getComputedStyle(canvas).getPropertyValue("height").slice(0, -2);
-    let style_width = getComputedStyle(canvas).getPropertyValue("width").slice(0, -2);
-    canvas.setAttribute('height', style_height * dpi);
-    canvas.setAttribute('width', style_width * dpi);
+    // Adjust canvas for device and content sizing
+    canvas_left.setAttribute("height", style_height * dpi);
+    canvas_left.setAttribute("width", style_width * dpi);
+    canvas_right.setAttribute("height", style_height * dpi);
+    canvas_right.setAttribute("width", style_width * dpi);
 
-    // Size canvas
-    let navbar_height = getComputedStyle($("#navbar")[0]).getPropertyValue("height");
-    $("#bg").css({top: `${navbar_height}`});
+    // Size canvas and content
+    let navbar_height = parseInt(getComputedStyle($("#navbar")[0]).getPropertyValue("height").slice(0, -2));
+    $("#bg_left").css({"margin-top": navbar_height});
+    $("#bg_right").css({"margin-top": navbar_height});
+    $("#main").css({"margin-top": navbar_height});
+    
+    if (do_animation) { 
+        animation_id = requestAnimationFrame(draw);
+    }
 }
 
 // Draw drops
 function draw() {
-    for (var i = 0; i < columns; i++) {
-        // Draw blue border
-        ctx.fillStyle = "#00BFFF";
-        ctx.fillRect(i * width, left_drops[i] * width, width, width);
-        // Draw inner black
-        ctx.fillStyle = "#000";
-        ctx.fillRect(i * width + 1, left_drops[i] * width + 1, width - 3, width - 3);
-        if (left_drops[i] * width > window.innerHeight && Math.random() > 0.975) {
-            left_drops[i] = 0;
+    if (frame == 0) {
+        for (var i = 0; i < columns; i++) {
+            drawSquare(ctx_left, i * width, left_drops[i] * width, width);
+            if (left_drops[i] * width > window.innerHeight && Math.random() > drop_spawn_rate) {
+                left_drops[i] = 0;
+            }
+            left_drops[i]++;
         }
-
-        left_drops[i]++;
+        for (var i = 0; i < columns; i++) {
+            drawSquare(ctx_right, i * width, right_drops[i] * width, width);
+            if (right_drops[i] * width > window.innerHeight && Math.random() > drop_spawn_rate) {
+                right_drops[i] = 0;
+            }
+            right_drops[i]++;
+        }    
     }
-    for (var i = 0; i < columns; i++) {
-        // Draw blue border
-        ctx.fillStyle = "#00BFFF";
-        ctx.fillRect(window.innerWidth - i * width, right_drops[i] * width, width, width);
-        // Draw inner black
-        ctx.fillStyle = "#000";
-        ctx.fillRect(window.innerWidth - (i * width + 1), right_drops[i] * width + 1, width - 3, width - 3);
-        if (right_drops[i] * width > window.innerHeight && Math.random() > 0.975) {
-            right_drops[i] = 0;
-        }
-
-        right_drops[i]++;
+    
+    frame++;
+    if (frame == 2) {
+        frame = 0;
     }
+    
+    animation_id = requestAnimationFrame(draw);
 }
 
-resizeCanvas();
-window.addEventListener("resize", resizeCanvas, false);
+function drawSquare(ctx, x, y, width, border_thickness = 2) {
+    let step = 0.1, gamma = 1.0;
+    for (var i = 0; gamma > 0; gamma -= step, i++) {
+        // Remove previous
+        ctx.fillStyle = "#000";
+        ctx.fillRect(x, y - (i * width), width, width);
+        // Blue square
+        ctx.fillStyle = `rgba(0, 255, 0, ${gamma})`; //`rgba(0, 191, 255, ${gamma})`;
+        ctx.fillRect(x, y - (i * width), width, width);
+        // Black fill
+        ctx.fillStyle = "#000";
+        ctx.fillRect(x + border_thickness, (y + border_thickness) - (i * width), width - (border_thickness * 2), width - (border_thickness * 2));
+    }   
+}
 
-setInterval(draw, 33);
+function clearColumn() {
+    // TODO
+}
+
+$(document).ready(resizeCanvas);
+$(window).bind("resize", resizeCanvas);
